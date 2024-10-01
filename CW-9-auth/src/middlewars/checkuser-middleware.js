11c0d0701aa5b910e26067ddcc17328fb9dcb2e0
BeloMaximka <belomaxim@gmail.com>
2024-10-01 17:25:18 +0300

    Completed auth homework

--- CW-9-auth/src/middlewars/checkuser-middleware.js
+++ CW-9-auth/src/middlewars/checkuser-middleware.js
@@ -1,5 +1,4 @@
 const checkUser = (req, res, next) => {
-  console.log(req.cookies);
   if (req.cookies && req.cookies.user) {
     res.locals.user = req.cookies.user;
   }
--- CW-9-auth/src/routes/user-routes.js
+++ CW-9-auth/src/routes/user-routes.js
@@ -1,29 +1,93 @@
 import { Router } from "express";
 import bcrypt from "bcrypt";
+import { checkSchema, validationResult } from "express-validator";
+
+const users = [];
+
 const userRoutes = Router();
 userRoutes.get("/user/signin", (req, res) => {
   res.render("form_auth");
 });
 
+userRoutes.post(
+  "/user/signin",
+  checkSchema({
+    login: {
+      isEmpty: { negated: true, errorMessage: "Login is required" },
+    },
+    password: {
+      isEmpty: { negated: true, errorMessage: "Password is required" },
+    },
+  }),
+  (req, res) => {
+    const user = users.find((user) => user.login === req.body.login);
+    if (!user) {
+      renderSignInWithError(res);
+      return;
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      renderSignInWithError(res);
+      return;
+    }
+    setUserCookie(res, user.login);
+    res.redirect("/");
+  }
+);
+
+function renderSignInWithError(res) {
+  res.render("form_auth", { error: "Login or password is incorrect" });
+}
 userRoutes.get("/user/signup", (req, res) => {
   res.render("form_register");
 });
-//реєстрація
-userRoutes.post("/user/signup", (req, res) => {
-  //TODO: валідація даних
-  /*
-    1) login, email, password, confirm_password
-    2) якщо все добре, записуєте користувача в масив(або файл)
-    */
-  //   const hash = bcrypt.hashSync(req.body.password, 10);
-  //   const old_hash =
-  //     "$2b$10$J9hYH1nGL9LoYncBDKGgBOoCQ69frW6VMId2EGjXMPoLlrgXBgD/y";
-  //   console.log(bcrypt.compareSync(req.body.password, old_hash));
 
-  res.cookie("user", req.body.login, {
+userRoutes.post(
+  "/user/signup",
+  checkSchema({
+    email: { isEmail: { errorMessage: "Invalid email" } },
+    password: {
+      isLength: {
+        options: { min: 8 },
+        errorMessage: "Password should contain at least 8 characters",
+      },
+    },
+    confirm_password: {
+      custom: { options: (value, data) => value === data.req.body.password },
+      errorMessage: "Passwords do not match",
+    },
+    login: {
+      isEmpty: { negated: true, errorMessage: "Login is required" },
+    },
+  }),
+  (req, res) => {
+    const result = validationResult(req).array();
+    if (result.length > 0) {
+      const errors = result.map((error) => error.msg);
+      res.render("form_register", { errors });
+      return;
+      
+    }
+
+    const hash = bcrypt.hashSync(req.body.password, 10);
+      users.push({
+        email: req.body.email,
+        login: req.body.login,
+        password: hash,
+      });
+      setUserCookie(res, req.body.login);
+      res.redirect("/");
+  }
+);
+
+function setUserCookie(res, token) {
+  res.cookie("user", token, {
     httpOnly: true,
     maxAge: 1000000,
   });
+}
+
+userRoutes.get("/user/logout", (req, res) => {
+  res.clearCookie("user");
   res.redirect("/");
 });
 
