5d802547d0f79710c5ae43830c0a803411c7655c
BeloMaximka <belomaxim@gmail.com>
2024-10-02 11:04:47 +0300

    Completed jwt homework

---
+++ HW-10-auth/users.js
@@ -0,0 +1,8 @@
+export const users = [
+    {
+      id: 1,
+      login: "vasya",
+      email: "vasya@gmail.com",
+      password: "$2b$10$9EbkD9UElCDWPZ11PRLviuIUyQHBoo9GzAF4H8LqGSiu48zS/7PPi", // 1Password!
+    },
+  ];
